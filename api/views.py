c448383122b68e631b97f0f5567599eaa6482cea
rcharkaoui <rayane.charkaoui@saint-remi.net>
2024-11-13 11:10:51 +0100

    Initial commit

---
+++ api/views.py
@@ -0,0 +1,10 @@
+from rest_framework import generics
+from django.shortcuts import render
+from .models import Room
+from .serializers import RoomSerializer
+
+
+# Create your views here.
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
